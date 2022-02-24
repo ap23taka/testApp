c51a0bee7f70d8edec07c8ecae6c7f861ddc6441
ap23taka <ap23taka@gmail.com>
2022-02-24 17:02:23 +0900

    commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+##
+st.title("Data to Pairplot APP")
+
+## Data
+st.subheader("Data Selection")
+uploaded_file = st.file_uploader(
+    label="Upload an excel file"
+)
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    # st.write(type(uploaded_file))
+    st.subheader("Uploaded Data")
+    st.write(df)
+    st.write(f"Columns : {df.columns.values}")
+
+    ### side bar
+    option = st.sidebar.multiselect(
+        label = 'Select columns for correlation plot.',
+        options=df.columns.values.tolist(),
+        default=df.columns.values[:4]
+    )
+
+    # plot
+    st.subheader('Pairplot (Matrix of Correlation plot)')
+    st.write("Select columns at side bar.")
+    st.write('Current selections :', option)
+
+    fig = sns.pairplot(data=df, vars=option, diag_kind="kde")
+    st.pyplot(fig)
+
+
+

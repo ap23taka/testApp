0b0cc0576478490ca6d46ac499d832d771dc7ff4
ap23taka <ap23taka@gmail.com>
2022-03-08 12:25:06 +0900

    update

--- app.py
+++ app.py
@@ -3,20 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-import streamlit_authenticator as stauth
-
-##### Basic Auth
-def basicauth_
-# configs
-names = ["user"]
-usernames = ["username"]
-# passwords = ["***"] # delete after hashing
-# hashed_passwords = stauth.hasher(passwords).generate()
-hashed_passwords = ['$2b$12$3N0xSkirQPcndDPSmQfKZOc2EEJByhJbv.mxm9cUWQDATsO4.fuNa']
-authenticator = stauth.authenticate(names,usernames,hashed_passwords,'some_cookie_name','some_signature_key',cookie_expiry_days=0)
-
-# Get User Input via APP interface
-name, authentication_status = authenticator.login('Login','main')
 
 ##### APP
 def main():
@@ -49,11 +35,6 @@ def main():
         fig = sns.pairplot(data=df, vars=option, diag_kind="kde")
         st.pyplot(fig)
 
-if authentication_status:
-    # st.write('Welcome *%s*' % (name))
-    main()
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
 
+if __name__ == '__main__':
+    main()
